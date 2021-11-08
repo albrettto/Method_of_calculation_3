6cd02d771fb77107557b1b4098236286cfc6ce93
khusamov Albert <khusamov01@mail.ru>
2021-11-08 11:01:18 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Method_of_calculation_3
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+            this.dtGdVw = new System.Windows.Forms.DataGridView();
+            this.rd_Btn1 = new System.Windows.Forms.RadioButton();
+            this.rd_Btn2 = new System.Windows.Forms.RadioButton();
+            this.rd_Btn3 = new System.Windows.Forms.RadioButton();
+            this.Result_btn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGdVw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtGdVw
+            // 
+            this.dtGdVw.BackgroundColor = System.Drawing.Color.FloralWhite;
+            this.dtGdVw.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGdVw.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dtGdVw.Location = new System.Drawing.Point(13, 13);
+            this.dtGdVw.Name = "dtGdVw";
+            this.dtGdVw.RowHeadersWidth = 51;
+            this.dtGdVw.RowTemplate.Height = 24;
+            this.dtGdVw.Size = new System.Drawing.Size(829, 466);
+            this.dtGdVw.TabIndex = 0;
+            // 
+            // rd_Btn1
+            // 
+            this.rd_Btn1.AutoSize = true;
+            this.rd_Btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rd_Btn1.Location = new System.Drawing.Point(13, 485);
+            this.rd_Btn1.Name = "rd_Btn1";
+            this.rd_Btn1.Size = new System.Drawing.Size(216, 24);
+            this.rd_Btn1.TabIndex = 1;
+            this.rd_Btn1.TabStop = true;
+            this.rd_Btn1.Text = "y=6x^5 на отрезке [0,1]";
+            this.rd_Btn1.UseVisualStyleBackColor = true;
+            // 
+            // rd_Btn2
+            // 
+            this.rd_Btn2.AutoSize = true;
+            this.rd_Btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rd_Btn2.Location = new System.Drawing.Point(589, 515);
+            this.rd_Btn2.Name = "rd_Btn2";
+            this.rd_Btn2.Size = new System.Drawing.Size(80, 24);
+            this.rd_Btn2.TabIndex = 2;
+            this.rd_Btn2.TabStop = true;
+            this.rd_Btn2.Text = "[0, 1.5]";
+            this.rd_Btn2.UseVisualStyleBackColor = true;
+            // 
+            // rd_Btn3
+            // 
+            this.rd_Btn3.AutoSize = true;
+            this.rd_Btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rd_Btn3.Location = new System.Drawing.Point(589, 545);
+            this.rd_Btn3.Name = "rd_Btn3";
+            this.rd_Btn3.Size = new System.Drawing.Size(111, 24);
+            this.rd_Btn3.TabIndex = 3;
+            this.rd_Btn3.TabStop = true;
+            this.rd_Btn3.Text = "[0.001, 1.5]";
+            this.rd_Btn3.UseVisualStyleBackColor = true;
+            // 
+            // Result_btn
+            // 
+            this.Result_btn.BackColor = System.Drawing.Color.Coral;
+            this.Result_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Result_btn.ForeColor = System.Drawing.Color.Black;
+            this.Result_btn.Location = new System.Drawing.Point(351, 504);
+            this.Result_btn.Name = "Result_btn";
+            this.Result_btn.Size = new System.Drawing.Size(153, 48);
+            this.Result_btn.TabIndex = 4;
+            this.Result_btn.Text = "Результат";
+            this.Result_btn.UseVisualStyleBackColor = false;
+            this.Result_btn.Click += new System.EventHandler(this.Result_btn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FloralWhite;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(589, 489);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "x^(1/30)*sqrt(1+x^2) на отрезке:";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "n";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 75;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Kd";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 175;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "d Точное";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 175;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "d Рунге";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 175;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "d Теоретическое";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 175;
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(855, 592);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rd_Btn2);
+            this.Controls.Add(this.Result_btn);
+            this.Controls.Add(this.rd_Btn3);
+            this.Controls.Add(this.rd_Btn1);
+            this.Controls.Add(this.dtGdVw);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторная работа №3";
+            ((System.ComponentModel.ISupportInitialize)(this.dtGdVw)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtGdVw;
+        private System.Windows.Forms.RadioButton rd_Btn1;
+        private System.Windows.Forms.RadioButton rd_Btn2;
+        private System.Windows.Forms.RadioButton rd_Btn3;
+        private System.Windows.Forms.Button Result_btn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Method_of_calculation_3
+{
+    public partial class Main_Form : Form
+    {
+        double J, h;
+        double eps = 1e-6;
+        int n, k = 4;
+        public Main_Form()
+        {
+
+            InitializeComponent();
+
+        }
+
+        public double funct(double x)
+        {
+            if (rd_Btn1.Checked)
+                return 6 * Math.Pow(x, 5);
+            else
+                return Math.Pow(x, (1.0 / 30.0)) * Math.Sqrt(1 + Math.Pow(x, 2));
+        }
+
+        public double calcParabol(double h, double a, double b)
+        {
+            double sum = 0;
+            sum = sum + funct(a) + funct(b);
+            double S = 0;
+            for (int i = 1; i < (b - a) / h; i++)
+            {
+                S += funct(i * h + a);
+            }
+            sum += 2 * S;
+            S = 0;
+            for (int i = 0; i < (b - a) / h; i++)
+            {
+                S += funct(i * h + a + h / 2);
+            }
+            sum += 4 * S;
+            sum *= h / 6;
+            return sum;
+        }
+        public double calcKD(double h, double a, double b)
+        {
+            return (calcParabol((h / 2), a, b) - calcParabol(h, a, b)) / (calcParabol((h / 4), a, b) - calcParabol((h / 2), a, b));
+        }
+        public double calcDR(double h, double a, double b)
+        {
+            return (calcParabol(h / 2, a, b) - calcParabol(h, a, b)) / (Math.Pow(2, k) - 1);
+        }
+        public double calcDT(double h, double c)
+        {
+            return c * Math.Pow(h, k);
+        }
+        public double calcDE(double h, double a, double b)
+        {
+            return J - calcParabol(h, a, b);
+        }
+        private void Result_btn_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            dtGdVw.Rows.Clear();
+            if (rd_Btn1.Checked)
+            {
+                a = 0;
+                b = 1;
+                J = 1;
+                double c = 1.0 / 4;
+
+                for (n = 1; n <= 65536; n *= 2)
+                {
+                    h = (b - a) / n;
+                    dtGdVw.Rows.Add(n, calcKD(h * 4, a, b), calcDE(h, a, b), calcDR(h * 2, a, b), calcDT(h, c));
+
+                    if (n == 1)
+                    {
+                        dtGdVw.Rows[0].Cells[1].Value = "-";
+                        dtGdVw.Rows[0].Cells[3].Value = "-";
+                    }
+                    if (n == 2)
+                        dtGdVw.Rows[1].Cells[1].Value = "-";
+                }
+            }
+
+            else
+            {
+                if (rd_Btn2.Checked)
+                {
+                    a = 0;
+                    b = 1.5;
+                    for (n = 2; Math.Abs(calcDR(((b - a) * 2 / n), a, b)) >= eps; n *= 2)
+                    {
+                        h = (b - a) / n;
+                        dtGdVw.Rows.Add(n, calcKD(h * 4, a, b), "-", calcDR(h * 2, a, b), "-");
+                        if (n == 2)
+                            dtGdVw.Rows[0].Cells[1].Value = "-";
+                    }
+                }
+
+                if (rd_Btn3.Checked)
+                {
+                    a = 0.001;
+                    b = 1.5;
+                    for (n = 2; Math.Abs(calcDR(((b - a) * 2 / n), a, b)) >= eps; n *= 2)
+                    {
+                        h = (b - a) / n;
+                        dtGdVw.Rows.Add(n, calcKD(h * 4, a, b), "-", calcDR(h * 2, a, b), "-");
+                        if (n == 2)
+                            dtGdVw.Rows[0].Cells[1].Value = "-";
+                    }
+                }
+            }
+        }
+    }
+}
